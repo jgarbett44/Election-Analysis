b5b706af9c8749286df6f28732e80aaa27ed72d3
John Garbett <90982384+jgarbett44@users.noreply.github.com>
2021-10-17 10:45:27 -0700

    Add files via upload

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -99,24 +99,35 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a for loop to get the county from the county dictionary.
-
+    for county_name in county_votes:
         # 6b: Retrieve the county vote count.
-
+        votes = county_votes[county_name]
         # 6c: Calculate the percentage of votes for the county.
-
+        vote_percentage = float(votes) / float(total_votes) * 100
+        county_results = (
+            f"{county_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
          # 6d: Print the county results to the terminal.
-
+        print(county_results)
          # 6e: Save the county votes to a text file.
-
+        txt_file.write(county_results)
          # 6f: Write an if statement to determine the winning county and get its vote count.
-
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_county = county_name
+            winning_percentage = vote_percentage
 
     # 7: Print the county with the largest turnout to the terminal.
-
+     winning_county_summary = (
+        f"-------------------------\n"
+        f"County with Largest Turnout: {winning_county}\n"
+        f"Vote Count: {winning_count:,}\n"
+        f"Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_county_summary)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(winning_county_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
